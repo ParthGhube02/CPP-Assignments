14ac64413a94fe7e2be9ca38df387986e20de3c0
Parth Ghube <ghubeparth@gmail.com>
2020-05-26 19:54:12 +0530

    Parth Initial Commit

---
+++ Input output.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float annualsalary;
+    cout << "Please enter your annua salary ";
+    cin >> annualsalary;
+    float monthlysalary = annualsalary / 12;
+    cout << "Your monthly salary is " << monthlysalary << endl;
+    cout << "You will earn " << annualsalary * 10;
+    char character = 'a';// used for letter variables
+    system("pause>0");
+}
